caac4330561dfbdc1b1ba50c053292f7b28e68b7
Christoph Grabo <asaaki@mannaz.cc>
2023-12-08 12:24:34 +0100

    Use lol_alloc for wasm build

--- src/bin/dat.rs
+++ src/bin/dat.rs
@@ -1,5 +1,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(target_arch = "wasm32")]
+use lol_alloc::{FreeListAllocator, LockedAllocator};
+
+#[cfg(target_arch = "wasm32")]
+#[global_allocator]
+static ALLOCATOR: LockedAllocator<FreeListAllocator> =
+    LockedAllocator::new(FreeListAllocator::new());
+
 fn main() {
     destruction_and_tranquility_game::run();
 }
